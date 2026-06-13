26b9a5e9f01fadd963266a372c129c8a859c5cf1
SSEGUYA SALIM MUKO <118248066+calmeoceans@users.noreply.github.com>
2026-06-13 14:39:26 +0300

    updated

--- ocean-tracers-net-app/server/src/routes/contact.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ContactController = require('../controllers/contactController');
-
-const contactController = new ContactController();
-
-router.post('/', contactController.handleContactFormSubmission.bind(contactController));
-
-module.exports = router;
